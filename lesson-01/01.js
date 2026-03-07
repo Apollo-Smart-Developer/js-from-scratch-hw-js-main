c1c992af1a6a7238f489b0dbade028e73e1976fd
Aleksey-Krylov-51 <kvins1@mail.ru>
2026-03-07 22:55:00 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+const maxSum = 20;
+
+while (sum < maxSum) {
+    sum++
+    console.log(sum)
+}
