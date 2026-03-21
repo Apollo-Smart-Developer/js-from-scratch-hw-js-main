19f900ae848b0e1c48366bbce3a5d94ab9a5260d
Aleksey-Krylov-51 <kvins1@mail.ru>
2026-03-21 17:07:29 +0300

    done TZ

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,18 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+let sumOne = 1;
+let sumTwo = 2;
+
+function calculateAverage(a, b) {
+    if (calculateAverage) {
+        let average = (a + b) / 2;
+        console.log('Среднее арифметическое: ' + sumOne);
+    } else {
+        console.log('Сорян, нет такой арфиметики');
+    }
+}
+calculateAverage (sumOne, sumTwo);
+
+
+
+
